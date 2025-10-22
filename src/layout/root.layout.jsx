9a2cb0b472f05ec06a36bc1f2b4e4b48e7f16982
Abhijith <abhijithm050@gmail.com>
2025-10-22 17:07:31 +0530

    fix: changed import in scroll

--- src/layout/root.layout.jsx
+++ src/layout/root.layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import ScrollToTop from '../utils/ScrollToTop'
+import {ScrollToTop} from '../utils/ScrollToTop'
 import { Navbar } from "../components/Navbar";
 import Footer from "../components/Footer";
 
